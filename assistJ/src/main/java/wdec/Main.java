bd2e0bf12f44ea600a979b4de89716e8f3bf4122
Filip <fbielecki1@gmail.com>
2015-05-24 23:12:30 +0200

    test

--- assistJ/src/main/java/wdec/Main.java
+++ assistJ/src/main/java/wdec/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
 //test githuba
+//test 123123
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
