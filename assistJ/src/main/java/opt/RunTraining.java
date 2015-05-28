2def4ac155bf4f1045efbffb7463290bfd50a951
Michał <m.nowot@gmail.com>
2015-05-28 03:58:11 +0200

    new model

--- assistJ/src/main/java/opt/RunTraining.java
+++ assistJ/src/main/java/opt/RunTraining.java
@@ -6,8 +6,8 @@ public class RunTraining {
 
 	public static void main(final String args[]) {
 		NeuralTraining training = new NeuralTraining();
-		training.trainPercSoldApprox(6e-3,"?:B->TANH->9:B->?");
-		training.trainUnitPriceApprox(5e-4,"?:B->TANH->5:B->?");
+		training.trainPercSoldApprox(5e-3,"?:B->TANH->7:B->?");
+		training.trainUnitPriceApprox(2e-4,"?:B->TANH->5:B->?");
 		Encog.getInstance().shutdown();
 
 	}
